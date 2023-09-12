8a22932535b8e9ff3be302b255485edbd36d1a27
osamasabry9 <osamasabry324@gmail.com>
2023-09-12 16:16:34 +0300

    added search view

--- lib/features/search/presentation/views/search_view.dart
+++ lib/features/search/presentation/views/search_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/search_view_body.dart';
+import '../widgets/search_view_body.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
