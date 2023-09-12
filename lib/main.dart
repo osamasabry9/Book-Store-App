bb9225520bead2becbf96dc8da9cbbe8f8a9fd3d
osamasabry9 <osamasabry324@gmail.com>
2023-09-12 16:17:05 +0300

    some edits in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/constants.dart';
+import 'core/utils/app_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,10 +12,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
-      home:  Scaffold(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
     );
   }
 }
