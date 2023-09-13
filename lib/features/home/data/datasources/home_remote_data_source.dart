89036b4dc9a74220dd77c25a5ca956ea25cc4f1a
osamasabry9 <osamasabry324@gmail.com>
2023-09-13 17:03:51 +0300

    create home remote data source and

---
+++ lib/features/home/data/datasources/home_remote_data_source.dart
@@ -0,0 +1,38 @@
+import '../../../../core/constants.dart';
+import '../../../../core/utils/api_service.dart';
+import '../../domain/entities/book_entity.dart';
+import '../models/book_model/book_model.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
+
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.getBooks(
+        endpoint: fetchFeaturedBooksUrl);
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
