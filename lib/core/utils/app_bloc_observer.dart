9aea95cf833433d66a77e810d500d8c97aa05ec4
osamasabry9 <osamasabry324@gmail.com>
2023-12-01 19:48:13 +0200

    add bloc observer

---
+++ lib/core/utils/app_bloc_observer.dart
@@ -0,0 +1,13 @@
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    log(change.toString());
+    super.onChange(bloc, change);
+  }
+
+}
