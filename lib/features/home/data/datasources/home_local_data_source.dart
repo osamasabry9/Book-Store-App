0a1cd48a10909b438be0638a4483f14b53a79de4
osamasabry9 <osamasabry324@gmail.com>
2023-12-05 11:16:33 +0200

    paginate local data fetch featured books

--- lib/features/home/data/datasources/home_local_data_source.dart
+++ lib/features/home/data/datasources/home_local_data_source.dart
@@ -4,23 +4,27 @@ import '../../../../core/constants.dart';
 import '../../domain/entities/book_entity.dart';
 
 abstract class HomeLocalDataSource {
- List<BookEntity>fetchFeaturedBooks();
+  List<BookEntity> fetchFeaturedBooks({int pageNumber = 0});
 
- List<BookEntity>fetchNewestBooks();
+  List<BookEntity> fetchNewestBooks();
 }
-
 
 class HomeLocalDataSourceImpl implements HomeLocalDataSource {
   @override
-  List<BookEntity> fetchFeaturedBooks() {
+  List<BookEntity> fetchFeaturedBooks({int pageNumber = 0}) {
+    int startIndex = pageNumber * 10;
+    int endIndex = startIndex + 10;
     var box = Hive.box<BookEntity>(kFeaturedBox);
-    return box.values.toList();
+    int length = box.values.length;
+    if (startIndex >= length || endIndex > length) {
+      return [];
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
   }
 
   @override
   List<BookEntity> fetchNewestBooks() {
-     var box = Hive.box<BookEntity>(kNewestBox);
+    var box = Hive.box<BookEntity>(kNewestBox);
     return box.values.toList();
   }
-
 }
